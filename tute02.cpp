4e19ad4eb37efc06f089b788fd349733203c136f
IT21284984 <99232495+it21284984@users.noreply.github.com>
2022-02-15 06:26:41 +0000

    complete

--- tute02.cpp
+++ tute02.cpp
@@ -12,17 +12,48 @@ Type = 3
 OtRate = 1700
 Please Note that the input command in C++ is std::cin. This is a representation of the Keyboard.*/
 
-#include <stdio.h>
+// #include <stdio.h>
+// int main()
+// {
+//    double salary, netSalary;
+//    int etype, otHrs, otRate;
+//    printf("Enter Employee Type : ");
+//    scanf("%d", &etype);
+//    printf("Enter Salary  : ");
+//    scanf("%f", &salary);
+//    printf("Enter OtHrs : ");
+//    scanf("%d", &otHrs); 
+   
+//    switch (etype) {
+//       case 1 :
+//           otRate = 1000;
+//           break;
+//       case 2 :
+//           otRate = 1500;
+//           break;
+//       default :
+//           otRate = 1700;
+//           break;
+//    }
+
+
+//    netSalary = salary + otHrs* otRate;
+//    printf("Net Salary is %f ", netSalary);
+  
+//    return 0;
+// }
+#include <iostream>
+using namespace std;
 int main()
 {
    double salary, netSalary;
    int etype, otHrs, otRate;
-   printf("Enter Employee Type : ");
-   scanf("%d", &etype);
-   printf("Enter Salary  : ");
-   scanf("%f", &salary);
-   printf("Enter OtHrs : ");
-   scanf("%d", &otHrs); 
+   cout<<"Enter Employee Type : ";
+   cin>>etype;
+   cout << "Enter Salary  : ";
+   cin >> salary;
+   cout << "Enter OtHrs : ";
+   cin >> otHrs;
    
    switch (etype) {
       case 1 :
@@ -36,9 +67,8 @@ int main()
           break;
    }
 
-
-   netSalary = salary + otHrs* otRate;
-   printf("Net Salary is %f ", netSalary);
+   netSalary = salary + otHrs* otRate; 
+   cout<<"Net Salary is"<<netSalary<<endl;
   
    return 0;
 }
